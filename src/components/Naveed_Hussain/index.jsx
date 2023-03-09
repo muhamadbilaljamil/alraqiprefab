6e5225e56aa56eab40de968a7ed04613f3664040
BiLaLJaMiLGitHub <bilaljamilgithub@gmail.com>
2023-03-09 11:38:01 +0500

    make social media link click able

--- src/components/Naveed_Hussain/index.jsx
+++ src/components/Naveed_Hussain/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import './Naveed_Hussain.css';
-import naveed_hussain from "../../assets/images/Naveed Hussain-1.png";
+import naveed_hussain from "../../assets/images/card.png";
+import instagram from "../../assets/images/instagram.png";
+import whatsapp from "../../assets/images/whatsapp.png";
+import facebook from "../../assets/images/facebook.png";
+import telegram from "../../assets/images/telegram.png";
+import twitter from "../../assets/images/twitter.png";
 
 const Naveed_Hussain = () => {
     return (
         <div className="naveed-hussain-card">
-            <img src={naveed_hussain} alt={"Naveed hussain"} />
+            <ul className="social-links">
+
+                <a href="https://api.whatsapp.com/send/?phone=%2B971585979780&text&type=phone_number&app_absent=0" target="_blank"><img src={whatsapp} alt="whatsapp link" className="social-link-item"/></a>
+                <a href="https://www.instagram.com/its.naveedqureshi/" target="_blank"><img src={instagram} alt="whatsapp link" className="social-link-item"/></a>
+                <a href="https://www.facebook.com/Naveed.KingsMaker" target="_blank"><img src={facebook} alt="whatsapp link" className="social-link-item"/></a>
+                <a href="https://twitter.com/fornaxnaveed" target="_blank"><img src={twitter} alt="whatsapp link" className="social-link-item"/></a>
+                <a href="https://t.me/fornaxchain" target="_blank"><img src={telegram} alt="whatsapp link" className="social-link-item"/></a>
+            </ul>
+            <img src={naveed_hussain} alt={"Naveed hussain"}/>
         </div>
     );
 }
